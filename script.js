a8e179c9538b331da72befa07a2a016181d8b986
mpo.coder <mpokodner@gmail.com>
2025-04-17 18:06:45 -0500

    javascript update

---
+++ script.js
@@ -0,0 +1,40 @@
+function updateTime() {
+  let laElement = document.querySelector("#LA");
+
+  if (laElement) {
+    let laDateElement = losAngeles.querySelector(".date");
+    let laTimeElement = losAngeles.querySelector(".time");
+    let laTime = moment().tz("America/Los_Angeles");
+
+    laDateElement.innerHTML = laTime.format("MMMM Do YYYY");
+    laTimeElement.innerHTML = laTime.format("h:mm:ss A");
+  }
+  let parisElement = document.querySelector("#Paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format("h:mm:ss A");
+  }
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = `div class="city">
+    <div>
+    <h2>${cityName}<h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    <div class="time">${cityTime.format("h:mm:ss A")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss A")}</div>
+    </div>`;
+}
+updateTime();
+setInterval(updateTime, 1000);
+let citiesSelectElement = document.querySelector("#city-select");
+citiesSelectElement.addEventListener("change", updateCity);
